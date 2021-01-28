9eb74812c8f5467efbc2c2db4729d1ec2ba09491
AnumehaSrivastava05 <anumeha.saran@gmail.com>
2021-01-27 17:59:16 -0700

    Handshake  server-client  code

---
+++ hnclient-v2/src/main/java/ca/bc/gov/hlth/hnclientv2/handshake/client/HandshakeClient.java
@@ -0,0 +1,147 @@
+package ca.bc.gov.hlth.hnclientv2.handshake.client;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+public class HandshakeClient {
+
+	private static Socket netSocket = null;
+
+	private static byte decodeSeed = 0;
+
+	// HandShake Segment
+	private static byte handShakeSegment[] = new byte[12];
+	private static byte handShakeData[] = new byte[8];
+
+	public static void main(String args[])
+      {
+	   final int SOCKET_READ_SLEEP_TIME = 100; // milliseconds
+	    final int MAX_SOCKET_READ_TRIES = 100; // total of 10 seconds
+	    int numSocketReadTries = 0;
+	   
+	   try{
+		    //Socket socketClient= new Socket("localhost",5555);
+	    System.out.println("Client: "+"Connection Established");
+	    Socket netSocket = new Socket("127.0.0.1", 5555);
+	    
+	    //netSocket = new Socket("127.0.0.1", 55286);
+	     BufferedInputStream socketInput =
+	          new BufferedInputStream(netSocket.getInputStream(), 1000);
+	      BufferedOutputStream socketOutput =
+	          new BufferedOutputStream(netSocket.getOutputStream());      
+	      
+	      System.out.println("waitng for HNClent");
+	   // wait for data from HNClient
+	      while (socketInput.available() < 1 && numSocketReadTries < MAX_SOCKET_READ_TRIES) {
+	        numSocketReadTries++;
+	        java.lang.Thread.sleep(SOCKET_READ_SLEEP_TIME);
+	        System.out.println("Waiting for Listener ");
+	      }
+
+	      // data available for read in BufferedInputStream
+	      if (socketInput.available() > 0) {    	  
+	        // read 12 bytes of HandShake Segment and 8 bytes of HandShake data
+	    	System.out.println("Reading handshake data client side");
+	        socketInput.read(handShakeSegment, 0, 12);	        
+	        socketInput.read(handShakeData, 0, 8);
+	      
+	        // write 12 bytes of HS response to BufferedOutputStream
+	        socketOutput.write("HS0000000008".getBytes(),0,12);
+	        System.out.println("Wrote HSSegment to listener");
+	        
+	        
+	     // write 8 bytes of scrambled HandShake data to BufferedOutputStream   
+	        socketOutput.write(scrambleData(handShakeData),0,8);  
+	     // set decodeSeed to last byte of scrambled handShakeData
+	     			decodeSeed = handShakeData[7];
+
+	       // System.out.println("Wrote HSData to listener");	
+	        socketOutput.flush();
+	        
+	        if (netSocket != null)
+			{
+				socketInput.close();
+				socketOutput.close();
+				netSocket.close();
+			}
+
+	      }
+			
+
+   }catch(Exception e)
+	   {e.printStackTrace();}
+      }
+
+	/**
+	 * <p>
+	 * The new HNClient 4.1.d expects data which it receives to be in scrambled from
+	 * This method scrambles incoming data (ex. HL7 message ) using exclusive OR and
+	 * a decodeSeed The same decodeSeed is used in unscrambling the data
+	 *
+	 * @param aByte array of byte
+	 * @return array of byte
+	 */
+	protected static byte[] scrambleData(byte[] aByte) {	
+		String s = new String(aByte, StandardCharsets.UTF_8);
+		System.out.println(s);
+		aByte[0] ^= decodeSeed;
+		for (int x = 1; x < aByte.length; x++) {
+			aByte[x] ^= aByte[x - 1];
+		}
+		
+		System.out.println(aByte);
+		return aByte;
+	}
+	
+	
+	/**
+	* This method overrides the finalize method.
+	* <p>
+	* It is a clean up method closing the socket if it is still open and destroying the timer thread if it is still
+	* running.
+	*/
+	protected void finalize() throws Throwable
+	{
+		if (netSocket != null)
+		{
+			try
+			{
+				netSocket.close();
+				netSocket = null;
+			}
+			catch (java.io.IOException e)
+			{
+			}
+		}
+		super.finalize();
+	}
+
+	
+	
+	/**
+	   * Insert the method's description here. Creation date: (2/6/01 4:38:50 PM) (JLee)
+	   *
+	   * <p>The new HNClient 4.1.d sends data in a scrambled form after the initial HandShaking data (12
+	   * bytes), and random 8 bytes (total 20 bytes) are sent This method unscrambles the data using
+	   * exclusive OR and a decode seed The decode seed is derived from the random set of 8 bytes (it is
+	   * also used in data scrambling)
+	   *
+	   * @param scrambleByte byte[]
+	   * @return
+	   */
+	  public String unScrambleData(byte[] scrambleByte) {
+
+	    byte prevByte = scrambleByte[0];
+	    scrambleByte[0] ^= decodeSeed;
+	    for (int x = 1; x < scrambleByte.length; x++) {
+	      byte currByte = scrambleByte[x];
+	      scrambleByte[x] ^= prevByte;
+	      prevByte = currByte;
+	    }
+	    return new String(scrambleByte);
+	  }
+	}
+
+
+
---
+++ hnclient-v2/src/main/java/ca/bc/gov/hlth/hnclientv2/handshake/server/HandshakeServer.java
@@ -0,0 +1,258 @@
+package ca.bc.gov.hlth.hnclientv2.handshake.server;
+
+import java.io.*;
+import java.net.*;
+import java.util.Random;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ca.bc.gov.hlth.hnclientv2.wrapper.Base64Encoder;
+
+class HandshakeServer {
+
+	private static int XFER_HANDSHAKE_SEED = 0;
+	private static int XFER_HANDSHAKE_SIZE = 8;
+	private static String XFER_HS_SEGMENT = "HS";
+	private static String HNET_RTRN_SUCCESS = "HNET_RTRN_SUCCESS";
+	private static String HNET_RTRN_INVALIDPARAMETER = "HNET_RTRN_INVALIDPARAMETER";
+	private static String HNET_RTRN_INVALIDFORMATERROR = "HNET_RTRN_INVALIDFORMATERROR";
+
+	private static Logger logger = LoggerFactory.getLogger(Base64Encoder.class);
+
+	public static void main(String args[]){
+
+		System.out.println(" Server is Running  ");
+		logger.debug("HandShakeServer is running");
+		try {
+		ServerSocket mysocket = new ServerSocket(5555);
+		
+			while (true) {
+				Socket connectionSocket = mysocket.accept();
+				BufferedInputStream socketInput = new BufferedInputStream(connectionSocket.getInputStream(), 1000);
+
+				BufferedOutputStream socketOutput = new BufferedOutputStream(connectionSocket.getOutputStream());
+				System.out.println(socketInput.available());
+
+				String ret_code = xfer_ReceiveHSSegment(socketOutput, socketInput, XFER_HANDSHAKE_SEED);
+				System.out.println("Handshake is done with the message: {}"+ ret_code);
+				logger.debug("Handshake is done with the message: {}", ret_code);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*----------------------------------------------------------------------------
+	 *This function computes the original handshake challenge data for the 
+	 * listener and sends this back to the originator. It then waits for the
+	 * originator's response, and when the response is received it is compared
+	 * with what is expected. If a match occurs, the computed handshake seed 
+	 * is returned
+	 *@return success/failure message
+	 */
+
+	protected static String xfer_ReceiveHSSegment(BufferedOutputStream bos, BufferedInputStream ios,
+			Integer handshakeSeed) throws Exception {
+		String retCode = HNET_RTRN_SUCCESS;
+
+		byte handshakeData[] = new byte[XFER_HANDSHAKE_SIZE];
+		byte clientHandshakeData[] = new byte[XFER_HANDSHAKE_SIZE];
+
+		if (handshakeSeed == null)
+			return HNET_RTRN_INVALIDPARAMETER;
+
+		/** Create the initial handshake data. **/
+
+		if (retCode.equals(HNET_RTRN_SUCCESS))
+			retCode = generateHandshakeData(handshakeData);
+
+		/** Now send the Handshake Segment Header. */
+
+		if (retCode.equals(HNET_RTRN_SUCCESS)) {
+			bos.write("HS0000000008".getBytes(), 0, 12);
+			bos.flush();
+		}
+
+		/** Now send the actual handshake data */
+
+		if (retCode == HNET_RTRN_SUCCESS) {
+			bos.write(handshakeData);
+			bos.flush();
+
+			System.out.println(" Sent HS Data Block to originator");
+
+		}
+
+		/*
+		 * Now receive and verify the originator's response to the handshake data. This
+		 * must be the first segment which the originator sends.
+		 */
+
+		if (retCode == HNET_RTRN_SUCCESS)
+			retCode = recvVerifyHandshakeData(ios, handshakeData, clientHandshakeData);
+
+		if (retCode.equals(HNET_RTRN_SUCCESS))
+			handshakeSeed = XFER_HANDSHAKE_SIZE - 1;
+		else
+			System.out.println("Error recieveing handshakedata");
+
+		return retCode;
+	}
+
+	protected static String recvVerifyHandshakeData(BufferedInputStream ios, byte[] handShakeData,
+			byte[] clientHandshakeData) throws IOException, InterruptedException {
+
+		String funcName = "RecvVerifyHandshakeData";
+		String retCode = HNET_RTRN_SUCCESS;
+		System.out.println("inside recvVerifyHandshakeData");
+
+		/*
+		 * Check parameters.
+		 */
+
+		if (handShakeData == null) {
+			logger.debug("OrigninalHandshakeData parameter cannot be NULL");
+			retCode = HNET_RTRN_INVALIDPARAMETER;
+			return retCode;
+
+		}
+
+		if (clientHandshakeData == null) {
+			logger.debug("ClientHandshakeData parameter cannot be NULL" + funcName + "HNET_RTRN_INVALIDPARAMETER");
+			retCode = HNET_RTRN_INVALIDPARAMETER;
+
+			return retCode;
+
+		}
+
+		/*
+		 * First get the originator handshake segment header and data.
+		 */
+		if (retCode == HNET_RTRN_SUCCESS) {
+
+			byte[] clientHandshakesegment = new byte[12];
+
+			// read and verify the handshake header
+			ios.read(clientHandshakesegment, 0, 12);
+
+			if (!compareByteArray(clientHandshakesegment, "HS0000000008".getBytes())) {
+				logger.debug("Handshake header segment are not matched");
+				return "HNET_RTRN_INVALIDFORMATERROR";
+			}
+
+			// read and verify the handshake data
+			ios.read(clientHandshakeData, 0, 8);
+			retCode = lcl_VerifyHandshakeResponse(clientHandshakeData, handShakeData, XFER_HANDSHAKE_SIZE);
+		}
+		return retCode;
+	}
+
+	protected static String lcl_VerifyHandshakeResponse(byte[] clientHandshakeData, byte[] originalHandshakeData,
+			int XFER_HANDSHAKE_SIZE) {
+		String retCode = HNET_RTRN_SUCCESS;
+
+		scrambleData(originalHandshakeData, XFER_HANDSHAKE_SIZE, XFER_HANDSHAKE_SEED, XFER_HS_SEGMENT);
+
+		if (!compareByteArray(clientHandshakeData, originalHandshakeData)) {
+			logger.debug("Received handshake data does not match expected data");
+			retCode = HNET_RTRN_INVALIDFORMATERROR;
+		} else
+			System.out.println("Received handshake data matched expected data");
+
+		return retCode;
+	}
+
+	protected static String generateHandshakeData(byte[] handShakeData) {
+		String ret_code = HNET_RTRN_SUCCESS;
+		if (handShakeData == null)
+			ret_code = HNET_RTRN_INVALIDPARAMETER;
+		else {
+			// create random object
+			Random r = new Random();
+			// put the next byte in the array
+			r.nextBytes(handShakeData);
+		}
+		return ret_code;
+	}
+
+	/**
+	 * Takes a data buffer and scramble the contents using a simple algorithm
+	 * 
+	 * @param dataBuffer   The buffer the contents of which are to be scrambled. May
+	 *                     not be NULL.
+	 * @param scrambleLen  The number of bytes in the dataBuffer parameter which are
+	 *                     to be scrambled. Must be <= the actual size of
+	 *                     dataBuffer.
+	 *
+	 * @param scrambleSeed The seed value used to start the scrambling process.
+	 * @param segmentType  What type of HL7XFER segment data is in dataBuffer. Must
+	 *                     be one of of the XFER_XX_SEGMENT values.
+	 * @param traceBuffer  The buffer to place diagnostic and tracing information.
+	 */
+
+	private static String scrambleData(byte[] dataBuffer, int scrambleLen, int scrambleSeed, String segmentType) {
+
+		String retCode = HNET_RTRN_SUCCESS;
+
+		/* Check the parameters for correctness. */
+		if (dataBuffer == null) {
+			return retCode = HNET_RTRN_INVALIDPARAMETER;
+		}
+
+		/*
+		 * Now scramble the data using a simple algorithm
+		 */
+
+		if (retCode == HNET_RTRN_SUCCESS) {
+			dataBuffer[0] ^= scrambleSeed;
+			for (int i = 1; i < scrambleLen; i++)
+				dataBuffer[i] ^= dataBuffer[i - 1];
+		}
+
+		return retCode;
+	}
+
+	/**
+	 * <p>
+	 * The new HNClient 4.1.d sends data in a scrambled form after the initial
+	 * HandShaking data (12 bytes), and random 8 bytes (total 20 bytes) are sent
+	 * This method unscrambles the data using exclusive OR and a decode seed The
+	 * decode seed is derived from the random set of 8 bytes (it is also used in
+	 * data scrambling)
+	 *
+	 * @param scrambleByte byte[]
+	 * @return
+	 */
+	public String unScrambleData(byte[] scrambleByte, int unscrambleLen, byte unscrambleSeed, String segmentType) {
+		if (scrambleByte == null)
+			return HNET_RTRN_INVALIDPARAMETER;
+
+		byte prevByte = scrambleByte[0];
+		scrambleByte[0] ^= unscrambleSeed;
+		for (int x = 1; x < scrambleByte.length; x++) {
+			byte currByte = scrambleByte[x];
+			scrambleByte[x] ^= prevByte;
+			prevByte = currByte;
+		}
+		return new String(scrambleByte);
+	}
+
+	public static boolean compareByteArray(byte[] a, byte[] a2) {
+		if (a == a2)
+			return true;
+		if (a == null || a2 == null)
+			return false;
+
+		int length = a.length;
+		if (a2.length != length)
+			return false;
+
+		for (int i = 0; i < length; i++)
+			if (a[i] != a2[i])
+				return false;
+
+		return true;
+	}
+
+}
