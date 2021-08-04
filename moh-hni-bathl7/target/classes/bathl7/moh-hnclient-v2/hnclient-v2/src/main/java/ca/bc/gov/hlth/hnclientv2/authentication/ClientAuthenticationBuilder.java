79c495e5e193244796ab8785c97bff776111a27b
AnumehaSrivastava05 <anumeha.saran@gmail.com>
2021-08-04 10:52:56 -0600

    Deleted target folder

--- moh-hni-bathl7/target/classes/bathl7/moh-hnclient-v2/hnclient-v2/src/main/java/ca/bc/gov/hlth/hnclientv2/authentication/ClientAuthenticationBuilder.java
+++
@@ -1,8 +0,0 @@
-package ca.bc.gov.hlth.hnclientv2.authentication;
-
-import com.nimbusds.oauth2.sdk.auth.ClientAuthentication;
-
-public interface ClientAuthenticationBuilder {
-
-    ClientAuthentication build();
-}
