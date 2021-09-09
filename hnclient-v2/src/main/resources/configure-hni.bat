b88cd91a8c5868251d4376d0544b0b40c177a70a
Pankaj Kathuria <pankaj.kathuria@cgi.com>
2021-09-09 10:40:32 +1000

    Updated message as per Shababri's recommendation

--- hnclient-v2/src/main/resources/configure-hni.bat
+++ hnclient-v2/src/main/resources/configure-hni.bat
@@ -9,4 +9,5 @@ set /p client-secret="Enter Client Secret: "
 setx MOH_HNCLIENT_ID "%client-id%"
 setx MOH_HNCLIENT_SECRET "%client-secret%"
 
+echo HNS-Client registered and ready for Connections
 pause
